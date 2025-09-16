56a1dda6099dd16a08913803a5f3b013ba0d9a88
azzhrsausan <sausanfarah007@gmail.com>
2025-09-17 00:36:43 +0700

    Tugas 3

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class Item(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)  # Nama item, misalnya "Jersey Home 2025"
     price = models.IntegerField()  # Harga item dalam rupiah
     description = models.TextField()  # Deskripsi lengkap item
@@ -10,3 +12,11 @@ class Item(models.Model):
 
     def __str__(self):
         return f"{self.name} - Rp{self.price}"
+
+
+    
+# class Employee(models.Model):
+#     name = models.CharField(max_length=255) #tipe datanya char ==> bisa nampung beberapa kata teteapi memiliki limit
+#     age = models.IntegerField()
+#     persona = models.TextField() #tipe datanya text ==> bisa nampung banyak kata dan dia bentuknya tuh kayak text box gitu
+
